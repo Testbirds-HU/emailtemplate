bb2dee77af8c7be605bb2fb682eff3cd6f2463fa
DecentM <DecentM@users.noreply.github.com>
2016-08-07 16:24:42 +0200

    Starting email building and tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,12 +8,13 @@ module.exports = function(grunt){
         	// task configuration goes here
 
 // BEGIN HTML
-/**/			inline: {
-					main: {
-						src: 'web/res/html/src/index.html',
-						dest: 'web/res/html/grunt/index.inline.html'
-					}
-				},
+		emailBuilder: {
+			main: {
+				files: {
+					'web/res/html/dist/index.html': 'web/res/html/src/index.html'
+				}
+			}
+		},
 // END HTML
 
 // BEGIN CSS
@@ -22,12 +23,12 @@ module.exports = function(grunt){
             			safe: true
             		},
             		maincss: {
-            			src: ['web/res/css/src/main.css'],
-            			dest: 'web/res/css/grunt/main.prfx.css'
+            			src: ['web/res/css/src/static.css'],
+            			dest: 'web/res/css/grunt/static.prfx.css'
             		},
 					criticalcss: {
-            			src: ['web/res/css/src/critical.css', 'web/res/css/src/customize.css'],
-            			dest: 'web/res/css/grunt/critical.prfx.css'
+            			src: ['web/res/css/src/dynamic.css', 'web/res/css/src/customize.css'],
+            			dest: 'web/res/css/grunt/dynamic.prfx.css'
             		},
             	},
 /**/		cssmin: {
@@ -37,57 +38,13 @@ module.exports = function(grunt){
 			},
 			main: {
 				files: {
-					'web/res/css/dist/main.min.css': ['web/res/css/grunt/main.prfx.css'],
-					'web/res/css/dist/critical.min.css': ['web/res/css/grunt/critical.prfx.css'],
+					'web/res/css/dist/static.min.css': ['web/res/css/grunt/static.prfx.css'],
+					'web/res/css/dist/dynamic.min.css': ['web/res/css/grunt/dynamic.prfx.css'],
 					'web/res/css/dist/bootstrap.min.css': ['bower_components/bootstrap/dist/css/bootstrap.min.css']
 				}
 			}
 		},
 // END CSS
-
-// BEGIN JS
-/**/			jsvalidate: {
-					options: {
-						globals: {},
-						esprimaOptions: {},
-						verbose: false
-					},
-					before_min: {
-						files: {
-							src: ['Gruntfile.js', 'web/res/js/src/*.js']
-						}
-					},
-					after_min: {
-						files: {
-							src: ['Gruntfile.js', 'web/res/js/dist/*.js']
-						}
-					}
-				},
-/**/			concat: {
-					options: {
-						separator: ';'
-					},
-					main: {
-						src: [
-							'web/res/js/grunt/main.bower.js',
-						],
-						dest: 'web/res/js/grunt/main.concat.js'
-					}
-				},
-/**/			uglify: {
-					options: {
-						mangle: false
-					},
-					target: {
-						files: {
-							'web/res/js/dist/main.min.js': ['web/res/js/grunt/main.concat.js'],
-							'web/res/js/dist/jquery.min.js': ['bower_components/jQuery/dist/jquery.min.js'],
-							'web/res/js/dist/bootstrap.min.js': ['bower_components/bootstrap/dist/js/bootstrap.min.js'],
-							'web/res/js/dist/instantclick.min.js': ['bower_components/instantclick/instantclick.js']
-						}
-					}
-				},
-// END JS
 
 // BEGIN MEDIA
 /**/			imagemin: {
@@ -109,13 +66,8 @@ module.exports = function(grunt){
 	// define the default task that executes when we run 'grunt' from inside the project
 	grunt.registerTask('default', [
 		'autoprefixer',
-		'inline',
 		'cssmin',
-		'inline',
-		'jsvalidate:before_min',
-		'concat',
-		'uglify',
-		'jsvalidate:after_min',
-		'newer:imagemin'
+		'newer:imagemin',
+		'emailBuilder'
 	]);
 };
